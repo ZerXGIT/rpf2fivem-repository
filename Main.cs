81fcafed65c2e2a593f1b8d1ddbc58cfd586ad2f
Avenze <contact.avenze@gmail.com>
2021-08-07 19:41:08 +0200

    Fix issue #3

--- Main.cs
+++ Main.cs
@@ -253,127 +253,6 @@ namespace rpf2fivem
             }
         }
 
-
-        // LOCAL FOLDER URL HASING HELPERS
-
-
-        static Char[] s_Base32Char = {
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-            'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-            'y', 'z', '0', '1', '2', '3', '4', '5'};
-
-        private static string ToBase32StringSuitableForDirName(byte[] buff)
-        {
-            StringBuilder sb = new StringBuilder();
-            byte b0, b1, b2, b3, b4;
-            int l, i;
-
-            l = buff.Length;
-            i = 0;
-
-            // Create l chars using the last 5 bits of each byte.  
-            // Consume 3 MSB bits 5 bytes at a time.
-
-            do
-            {
-                b0 = (i < l) ? buff[i++] : (byte)0;
-                b1 = (i < l) ? buff[i++] : (byte)0;
-                b2 = (i < l) ? buff[i++] : (byte)0;
-                b3 = (i < l) ? buff[i++] : (byte)0;
-                b4 = (i < l) ? buff[i++] : (byte)0;
-
-                // Consume the 5 Least significant bits of each byte
-                sb.Append(s_Base32Char[b0 & 0x1F]);
-                sb.Append(s_Base32Char[b1 & 0x1F]);
-                sb.Append(s_Base32Char[b2 & 0x1F]);
-                sb.Append(s_Base32Char[b3 & 0x1F]);
-                sb.Append(s_Base32Char[b4 & 0x1F]);
-
-                // Consume 3 MSB of b0, b1, MSB bits 6, 7 of b3, b4
-                sb.Append(s_Base32Char[(
-                    ((b0 & 0xE0) >> 5) |
-                    ((b3 & 0x60) >> 2))]);
-
-                sb.Append(s_Base32Char[(
-                    ((b1 & 0xE0) >> 5) |
-                    ((b4 & 0x60) >> 2))]);
-
-                // Consume 3 MSB bits of b2, 1 MSB bit of b3, b4
-
-                b2 >>= 5;
-
-                if ((b3 & 0x80) != 0)
-                    b2 |= 0x08;
-                if ((b4 & 0x80) != 0)
-                    b2 |= 0x10;
-
-                sb.Append(s_Base32Char[b2]);
-
-            } while (i < l);
-
-            return sb.ToString();
-        }
-
-        public static string GetLocalAppDataUserConfigPathNoUserConfigFolder(string fullExePath)
-        {
-            //E.g.: fullExePath = @"C:\Program Files (x86)\MyExeFolder\MyProgram.exe"
-            var localAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-
-            var versionInfo = FileVersionInfo.GetVersionInfo(fullExePath);
-            var companyName = versionInfo.CompanyName;
-            var exeName = versionInfo.OriginalFilename;// or 'AppDomain.CurrentDomain.FriendlyName'
-
-            var assemblyName = AssemblyName.GetAssemblyName(fullExePath);
-            var version = assemblyName.Version.ToString();
-
-            var uri = "file:///" + fullExePath; //or 'assemblyName.CodeBase' if vshost (you can check the 'FriendlyName')
-            uri = uri.ToUpperInvariant();
-
-            var ms = new MemoryStream();
-            var bSer = new BinaryFormatter();
-            bSer.Serialize(ms, uri);
-            ms.Position = 0;
-            var sha1 = new SHA1CryptoServiceProvider();
-            var hash = sha1.ComputeHash(ms);
-            var hashstring = ToBase32StringSuitableForDirName(hash);
-
-            //<AppData Local User Path> + <Company Name> + <[ExeName]_[eid]_[Hash]> + <Version> + user.config
-            var userConfigLocalAppDataPath = Path.Combine(localAppDataPath, "GTAUtil", companyName, exeName + "_Url_" + hashstring, version);
-
-            return userConfigLocalAppDataPath;
-        }
-
-        public static string GetExeLocalAppDataUserConfigPath(string fullExePath)
-        {
-            //E.g.: fullExePath = @"C:\Program Files (x86)\MyExeFolder\MyProgram.exe"
-            var localAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-
-            var versionInfo = FileVersionInfo.GetVersionInfo(fullExePath);
-            var companyName = versionInfo.CompanyName;
-            var exeName = versionInfo.OriginalFilename;// or 'AppDomain.CurrentDomain.FriendlyName'
-
-            var assemblyName = AssemblyName.GetAssemblyName(fullExePath);
-            var version = assemblyName.Version.ToString();
-
-            var uri = "file:///" + fullExePath; //or 'assemblyName.CodeBase' if vshost (you can check the 'FriendlyName')
-            uri = uri.ToUpperInvariant();
-
-            var ms = new MemoryStream();
-            var bSer = new BinaryFormatter();
-            bSer.Serialize(ms, uri);
-            ms.Position = 0;
-            var sha1 = new SHA1CryptoServiceProvider();
-            var hash = sha1.ComputeHash(ms);
-            var hashstring = ToBase32StringSuitableForDirName(hash);
-
-            //<AppData Local User Path> + <Company Name> + <[ExeName]_[eid]_[Hash]> + <Version> + user.config
-            var userConfigLocalAppDataPath = Path.Combine(localAppDataPath, "GTAUtil", companyName, exeName + "_Url_" + hashstring, version, "user.config");
-
-            return userConfigLocalAppDataPath;
-        }
-
-
         private void Form1_Load(object sender, EventArgs e)
         {
             this.ActiveControl = label1; // prevent random textbox focus
@@ -642,11 +521,6 @@ namespace rpf2fivem
 
         }
 
-        private void placeHolderTextBox1_TextChanged(object sender, EventArgs e) //!gta5mods_tb.Text.Contains("7z")
-        {
-            
-        }
-
         private void btnAddQueue_Click(object sender, EventArgs e)
         {
             LogAppend("Job added!");
@@ -831,39 +705,6 @@ namespace rpf2fivem
             }
         }
 
-        async Task makeGtautilFolder(string gtaUtilTempFolder, string gtafolder)
-        {
-            xmlTemplate = $@"<?xml version=""1.0"" encoding=""utf-8""?>
-<configuration>
-    <configSections>
-        <sectionGroup name=""userSettings"" type=""System.Configuration.UserSettingsGroup, System, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"" >
-            <section name=""GTAUtil.Settings"" type=""System.Configuration.ClientSettingsSection, System, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"" allowExeDefinition=""MachineToLocalUser"" requirePermission=""false"" />
-        </sectionGroup>
-    </configSections>
-    <userSettings>
-        <GTAUtil.Settings>
-            <setting name=""GTAFolder"" serializeAs=""String"">
-                <value>{gtafolder}</value>
-            </setting>
-        </GTAUtil.Settings>
-    </userSettings>
-</configuration>";
-
-
-            try
-            {
-                Directory.CreateDirectory(GetLocalAppDataUserConfigPathNoUserConfigFolder(Path.Combine(Directory.GetCurrentDirectory(), @"lib\gtautil\GTAUtil.exe")));
-                await Task.Delay(1000);
-                //MessageBox.Show(gtaUtilTempFolder);
-                File.WriteAllText(gtaUtilTempFolder, xmlTemplate, Encoding.Default);
-            }
-            catch
-            {
-                //error handling
-            }
-
-
-        }
 
         private async void button2_Click_1(object sender, EventArgs e)
         {
